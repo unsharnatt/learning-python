7930fa8dbd39810edfdff62ccd1e578c6c4a3c9e
unsharnatt <48174985+unsharnatt@users.noreply.github.com>
2023-05-23 13:53:30 +0100

    Update 02_receiving_input.py

--- 02_receiving_input.py
+++ 02_receiving_input.py
@@ -4,5 +4,9 @@ import datetime
 today = datetime.date.today()
 year = today.year
 
-birth_year = int(input('Birth year: '))# converting from string input into int variable
+birth_year = int(input('Birth year: '))# casting from string input into int variable
 print(f'Your birth year is: {birth_year}, You are {year - birth_year}.')
+birth_year = str(birth_year)
+print(birth_year) 
+birth_year = float(birth_year)
+print(birth_year)
